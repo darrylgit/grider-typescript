bfffd0940a8b741b001a3b1cf5f5f4d61b222b50
darrylgit <darrylsmithup@gmail.com>
2020-12-26 23:37:02 -0500

    Add todo interface

--- fetchjson/index.ts
+++ fetchjson/index.ts
@@ -2,6 +2,22 @@ import axios from 'axios';
 
 const url = `https://jsonplaceholder.typicode.com/todos/1`;
 
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(res => {
-  console.log(res.data);
+  const todo = res.data as Todo;
+
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
+
+  console.log(`
+    The Todo with ID: ${id}
+    Has a title of: ${title}
+    It is ${completed ? '' : 'not '}completed.
+  `);
 });

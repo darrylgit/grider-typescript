fc0a05fc9c96c1e8c6a82fee7a90e738f561b7e7
darrylgit <darrylsmithup@gmail.com>
2020-12-28 22:47:49 -0500

    Features lectures pt.2

--- features/annotations/variables.ts
+++ features/annotations/variables.ts
@@ -22,3 +22,32 @@ let point: { x: number; y: number } = {
   x: 10,
   y: 20
 };
+
+// Function
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
+};
+
+// When to use annotations:
+// 1) When a function returns any
+const json = '{"x": 10, "y": 20}';
+const coordinates: { x: number; y: number } = JSON.parse(json);
+
+// 2) When we declare a variable on one line and initialize it later
+let words = ['red', 'green', 'blue'];
+let foundWord: boolean;
+
+for (let i = 0; i < words.length; i++) {
+  if (words[i] === 'green') {
+    foundWord = true;
+  }
+}
+
+// 3) Variable whose type cannot be inferred correctly
+let numbers = [-10, -1, 12];
+let numberAboveZero: number | boolean = false;
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > 0) {
+    numberAboveZero = numbers[i];
+  }
+}

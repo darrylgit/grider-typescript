c0a76550138827a0e2f6344b42c9abd68d27b202
darrylgit <darrylsmithup@gmail.com>
2021-11-06 16:09:01 -0400

    Create class for Google Map

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,15 @@
+/// <reference types="@types/google.maps" />
+// import { User } from './User';
+// import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 import { User } from './User';
-import { Company } from './Company';
 
+// const user = new User();
+// const company = new Company();
+
+// console.log(user);
+// console.log(company);
 const user = new User();
-const company = new Company();
+const customMap = new CustomMap('map');
 
-console.log(user);
-console.log(company);
+customMap.addUserMarker(user);

c52ef880ba55f27a82418d0ada37f174a8e121c4
darrylgit <darrylsmithup@gmail.com>
2021-11-07 21:31:03 -0500

    Initialize sorting app

---
+++ sort/build/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () { };
+    return Sorter;
+}());
+var sorter = new Sorter([10, 3, -5, 0]);
---
+++ sort/src/index.ts
@@ -0,0 +1,7 @@
+class Sorter {
+  constructor(public collection: number[]) {}
+
+  sort(): void {}
+}
+
+const sorter = new Sorter([10, 3, -5, 0]);

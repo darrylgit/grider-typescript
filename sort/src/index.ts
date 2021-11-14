f3741741ea2a2b473df99679f99ba8711499c6e5
darrylgit <darrylsmithup@gmail.com>
2021-11-14 00:20:36 -0500

    Refactor Sorter as abstract class

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,25 +1,21 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
 
 const charactersCollection = new CharactersCollection('eefaDEFAefmzp');
-const charSorter = new Sorter(charactersCollection);
-charSorter.sort();
+charactersCollection.sort();
 console.log(charactersCollection.data);
 
 const linkedList = new LinkedList();
 linkedList.add(1);
-linkedList.add(-10);
+linkedList.add(-11);
 linkedList.add(25);
 linkedList.add(50);
 linkedList.add(-5);
 linkedList.add(3);
-const linkedListSorter = new Sorter(linkedList);
-linkedListSorter.sort();
+linkedList.sort();
 linkedList.print();

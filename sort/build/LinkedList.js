f4bc21dba16918ebd4fb32964cfb859d5c0f55de
darrylgit <darrylsmithup@gmail.com>
2021-11-13 22:04:15 -0500

    Implement CharactersCollection

---
+++ sort/build/LinkedList.js
@@ -0,0 +1,61 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LinkedList = exports.Node = void 0;
+var Node = /** @class */ (function () {
+    function Node(data) {
+        this.data = data;
+        this.next = null;
+    }
+    return Node;
+}());
+exports.Node = Node;
+var LinkedList = /** @class */ (function () {
+    function LinkedList() {
+        this.head = null;
+    }
+    LinkedList.prototype.add = function (data) {
+        var node = new Node(data);
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+        var tail = this.head;
+        while (tail.next) {
+            tail = tail.next;
+        }
+        tail.next = node;
+    };
+    Object.defineProperty(LinkedList.prototype, "length", {
+        get: function () {
+            if (!this.head) {
+                return 0;
+            }
+            var length = 1;
+            var node = this.head;
+            while (node.next) {
+                length++;
+                node = node.next;
+            }
+            return length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    LinkedList.prototype.at = function (index) {
+        if (!this.head) {
+            throw new Error('Index out of bounds');
+        }
+        var counter = 0;
+        var node = this.head;
+        while (node) {
+            if (counter === index) {
+                return node;
+            }
+            counter++;
+            node = node.next;
+        }
+        throw new Error('Index out of bounds');
+    };
+    return LinkedList;
+}());
+exports.LinkedList = LinkedList;
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('eefaDEFAefmzp');
+const charSorter = new Sorter(charactersCollection);
+charSorter.sort();
+console.log(charactersCollection.data);

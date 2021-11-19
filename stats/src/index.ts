df1388b6451278981454545c1b25aa1a21e1b92a
darrylgit <darrylsmithup@gmail.com>
2021-11-18 23:34:02 -0500

    Init stats project, parse data

---
+++ stats/src/index.ts
@@ -0,0 +1,10 @@
+import fs from 'fs';
+
+const matches = fs
+  .readFileSync('football.csv', {
+    encoding: 'utf-8'
+  })
+  .split('\n')
+  .map((row: string): string[] => row.split(','));
+
+console.log(matches);
